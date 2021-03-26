68b6dd23f69ab47ceb47ac471512cec91371a9d5
gunish.jha <gunish.jha.master@Gmail.com>
2021-03-27 00:48:51 +0530

    Initial commit with Readme

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Singleton_Scala_Design_Pattern"
+
+version := "0.1"
+
+scalaVersion := "2.13.5"
---
+++ src/main/scala/App.scala
@@ -0,0 +1,6 @@
+object App extends App{
+  var s1 = Singleton
+  var s2 = Singleton
+  print(s1==s2)
+
+}
